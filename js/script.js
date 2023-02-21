bccdf76799afacf174178f8b608a84c2718f239f
Vladimir <zubietsuk@mail.ru>
2023-02-21 17:43:57 +0300

    Training

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-number = 10;
-console.log (number);
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
